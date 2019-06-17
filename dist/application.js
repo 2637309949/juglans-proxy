04a6395934180c4a5c13683118b11f17a61c4da0
double <2637309949@qq.com>
2019-06-17 15:48:18 +0800

    style: mit

--- dist/application.js
+++ dist/application.js
@@ -1,34 +1,23 @@
 "use strict";
 
-function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
-
-function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
-
-/**
- * @author [Double]
- * @email [2637309949@qq.com]
- * @create date 2019-01-09 16:55:19
- * @modify date 2019-01-09 16:55:19
- * @desc [koa proxy]
- */
+// Copyright (c) 2018-2020 Double.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+// The whole point of route-specific middleware is to run middleware for a specific route.
+// Otherwise there would be no difference between app.use() and router.use().
+// If you want middleware to run regardless of any route matching you can mount it on the app via app.use().
 // app.use(proxy({
 //   host:  'http://xxx.com',
 //   match: /^\/static\//,
 //   map: function(path) { return 'public/' + path; },
 // }));
+const convert = require('koa-convert');
+
 const proxy = require('koa-proxy');
 
-module.exports = params =>
-/*#__PURE__*/
-function () {
-  var _ref2 = _asyncToGenerator(function* (_ref) {
-    let {
-      router
-    } = _ref;
-    router.use(proxy(params));
-  });
-
-  return function (_x) {
-    return _ref2.apply(this, arguments);
-  };
-}();
+module.exports = params => (_ref) => {
+  let {
+    httpProxy
+  } = _ref;
+  httpProxy.use(convert(proxy(params)));
+};
