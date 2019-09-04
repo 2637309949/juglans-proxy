821fd23aae0eefb7e3fa85000dc4e2e993ec3961
double <2637309949@qq.com>
2019-09-04 17:51:29 +0800

    feat: 优化代码

--- dist/application.js
+++ dist/application.js
@@ -3,15 +3,16 @@
 // Copyright (c) 2018-2020 Double.  All rights reserved.
 // Use of this source code is governed by a MIT style
 // license that can be found in the LICENSE file.
-const convert = require('koa-convert');
+const proxy = require('./proxy');
 
-const proxy = require('koa-proxy');
+var assert = require('assert');
 
 function Proxy(options) {
   if (!(this instanceof Proxy)) {
     return new Proxy(options);
   }
 
+  assert.ok(options.host || options.map || options.url, 'should provide host, map or url');
   this.options = options;
 }
 
@@ -19,7 +20,7 @@ Proxy.prototype.plugin = function (_ref) {
   let {
     httpProxy
   } = _ref;
-  httpProxy.use(convert(proxy(this.options)));
+  httpProxy.use(proxy(this.options));
 };
 
 module.exports = Proxy;
